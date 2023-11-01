7e1b2e0621bb3665f5046b76def5cb10a6be6532
nomanfc <nomanfcb@gmail.com>
2023-11-01 17:08:50 +0600

    GalleryApp js component has been created

---
+++ src/components/Gallery/GalleryApp.js
@@ -0,0 +1,9 @@
+import styles from "../../styles/gallery.module.css";
+
+export default function GalleryApp() {
+  return (
+    <div className={styles.outerContainer}>
+      <div className={styles.mainContainer}>GalleryApp</div>
+    </div>
+  );
+}
