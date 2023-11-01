5e4e569163cda311e49dfb493beaeab7f14220ad
nomanfc <nomanfcb@gmail.com>
2023-11-01 19:43:52 +0600

    ImageGrid component added

---
+++ src/components/Gallery/ImageGrids.jsx
@@ -0,0 +1,9 @@
+import styles from "../../styles/imageGrids.module.css";
+
+export default function ImageGrids() {
+  return (
+    <div className={styles.imageGrid}>
+      <div className={styles.imageContainer}> Container </div>
+    </div>
+  );
+}
