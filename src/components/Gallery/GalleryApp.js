289a99b1cd7892864bc19ac3696826c68e11cd90
nomanfc <nomanfcb@gmail.com>
2023-11-01 18:01:24 +0600

    GalleryApp layout updated

--- src/components/Gallery/GalleryApp.js
+++ src/components/Gallery/GalleryApp.js
@@ -3,7 +3,14 @@ import styles from "../../styles/gallery.module.css";
 export default function GalleryApp() {
   return (
     <div className={styles.outerContainer}>
-      <div className={styles.mainContainer}>GalleryApp</div>
+      <div className={styles.mainContainer}>
+        <div className={styles.navbar}>
+          Nav here
+        </div>
+        <div className={styles.imageGrid}>
+          Image Grids
+        </div>
+      </div>
     </div>
   );
 }
