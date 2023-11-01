69671c587558753a276562170d5af584abfc753c
nomanfc <nomanfcb@gmail.com>
2023-11-01 19:44:26 +0600

    Navbar component added

---
+++ src/components/Gallery/NavBar.jsx
@@ -0,0 +1,9 @@
+import styles from "../../styles/navBar.module.css";
+
+export default function NavBar() {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.navbarInnerContainer}>HEELLL</div>
+    </div>
+  );
+}
