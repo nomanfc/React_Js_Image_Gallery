917c1780713d611702c70a5406ee5b38473f5c1e
nomanfc <nomanfcb@gmail.com>
2023-11-01 19:45:14 +0600

    GalleryApp component updated

--- src/components/Gallery/GalleryApp.jsx
+++ src/components/Gallery/GalleryApp.jsx
@@ -1,15 +1,13 @@
 import styles from "../../styles/gallery.module.css";
+import ImageGrids from "./ImageGrids";
+import NavBar from "./NavBar";
 
 export default function GalleryApp() {
   return (
     <div className={styles.outerContainer}>
       <div className={styles.mainContainer}>
-        <div className={styles.navbar}>
-          Nav here
-        </div>
-        <div className={styles.imageGrid}>
-          Image Grids
-        </div>
+        <NavBar/>
+        <ImageGrids/>
       </div>
     </div>
   );
