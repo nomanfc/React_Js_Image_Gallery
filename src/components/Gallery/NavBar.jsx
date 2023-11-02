3c8a14ecd6c2dd79617b5bf3cecfaad01ac99fec
nomanfc <nomanfcb@gmail.com>
2023-11-02 16:45:16 +0600

    Navbar style has been changed

--- src/components/Gallery/NavBar.jsx
+++ src/components/Gallery/NavBar.jsx
@@ -2,8 +2,8 @@ import styles from "../../styles/navBar.module.css";
 
 export default function NavBar() {
   return (
-    <div className={styles.navbar}>
-      <div className={styles.navbarInnerContainer}>HEELLL</div>
+    <div>
+      <div>HELLO</div>
     </div>
   );
 }
