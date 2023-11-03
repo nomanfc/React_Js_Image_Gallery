be3aefa351d4aca2d71fe5c53fe85356973fe93b
nomanfc <nomanfcb@gmail.com>
2023-11-03 22:07:27 +0600

    Image Card has been created

--- src/components/Gallery/ImageGrids.jsx
+++ src/components/Gallery/ImageGrids.jsx
@@ -1,9 +1,51 @@
-import styles from "../../styles/imageGrids.module.css";
+import React from "react";
+import { useDrag, useDrop } from "react-dnd";
+import styles from "../../styles/gallery.module.css"
 
-export default function ImageGrids() {
+export default function ImageCard ({ src, title, id, index, moveImage }) {
+  const ref = React.useRef(null);
+  const [, drop] = useDrop({
+    accept: "image",
+    hover: (item, monitor) => {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+      moveImage(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    }
+  });
+  const [{ isDragging }, drag] = useDrag({
+    type: "image",
+    item: () => {
+      return { id, index };
+    },
+    collect: (monitor) => {
+      return {
+        isDragging: monitor.isDragging()
+      };
+    }
+  });
+  const opacity = isDragging ? 0 : 1;
+  drag(drop(ref));
   return (
-    <div className={styles.imageGrid}>
-      <div className={styles.imageContainer}> Container </div>
+    <div ref={ref} style={{ opacity }} className={styles.gridItem}>
+      <img src={src} alt={title} />
     </div>
   );
-}
+};
