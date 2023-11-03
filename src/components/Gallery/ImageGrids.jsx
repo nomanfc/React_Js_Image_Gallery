d6dd51c27a8e2799f8ebd2cba7584f680ac8345f
nomanfc <nomanfcb@gmail.com>
2023-11-03 23:31:05 +0600

    ImageGrids refactored

--- src/components/Gallery/ImageGrids.jsx
+++ src/components/Gallery/ImageGrids.jsx
@@ -1,51 +1,71 @@
 import React from "react";
 import { useDrag, useDrop } from "react-dnd";
-import styles from "../../styles/gallery.module.css"
+import styles from "../../styles/gallery.module.css";
 
-export default function ImageCard ({ src, title, id, index, moveImage }) {
+export default function ImageCard({
+  src,
+  title,
+  id,
+  index,
+  moveImage,
+  selectedImages,
+  toggleImageSelection,
+}) {
   const ref = React.useRef(null);
+
   const [, drop] = useDrop({
     accept: "image",
     hover: (item, monitor) => {
-      if (!ref.current) {
-        return;
-      }
       const dragIndex = item.index;
-      const hoverIndex = index;
-      if (dragIndex === hoverIndex) {
+      if (dragIndex === index) {
         return;
       }
+      const hoverIndex = index;
       const hoverBoundingRect = ref.current?.getBoundingClientRect();
       const hoverMiddleY =
         (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
       const clientOffset = monitor.getClientOffset();
       const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return;
+
+      if (
+        (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) ||
+        (dragIndex > hoverIndex && hoverClientY > hoverMiddleY)
+      ) {
+        moveImage(dragIndex, hoverIndex);
+        item.index = hoverIndex;
       }
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return;
-      }
-      moveImage(dragIndex, hoverIndex);
-      item.index = hoverIndex;
-    }
+    },
   });
+
   const [{ isDragging }, drag] = useDrag({
     type: "image",
-    item: () => {
-      return { id, index };
-    },
-    collect: (monitor) => {
-      return {
-        isDragging: monitor.isDragging()
-      };
-    }
+    item: { id, index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
   });
+
   const opacity = isDragging ? 0 : 1;
   drag(drop(ref));
+
+  const isSelected = selectedImages.includes(id);
+
   return (
     <div ref={ref} style={{ opacity }} className={styles.gridItem}>
-      <img src={src} alt={title} />
+      <div className={styles.checkbox}>
+        <input
+          type="checkbox"
+          checked={isSelected}
+          onChange={() => toggleImageSelection(id)}
+        />
+      </div>
+      <img
+        src={src}
+        alt={title}
+        height="100%"
+        width="100%"
+        style={{ borderRadius: "10px" }}
+      />
     </div>
   );
-};
+}
