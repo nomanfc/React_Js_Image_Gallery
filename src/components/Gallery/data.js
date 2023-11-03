6339bfaba1970c915388ad75483bba0089793b3d
nomanfc <nomanfcb@gmail.com>
2023-11-03 23:31:20 +0600

    Image path updated

--- src/components/Gallery/data.js
+++ src/components/Gallery/data.js
@@ -2,56 +2,56 @@ export default [
   {
     id: 1,
     title: "image-1",
-    img: "/public/images/image-1.webp",
+    img: "/images/image-1.webp",
   },
   {
     id: 2,
     title: "image-2",
-    img: "/public/images/image-2.webp",
+    img: "/images/image-2.webp",
   },
   {
     id: 3,
     title: "image-3",
-    img: "/public/images/image-3.webp",
+    img: "/images/image-3.webp",
   },
   {
     id: 4,
     title: "image-4",
-    img: "/public/images/image-4.webp",
+    img: "/images/image-4.webp",
   },
   {
     id: 5,
     title: "image-5",
-    img: "/public/images/image-5.webp",
+    img: "/images/image-5.webp",
   },
   {
     id: 6,
     title: "image-6",
-    img: "/public/images/image-6.webp",
+    img: "/images/image-6.webp",
   },
   {
     id: 7,
     title: "image-7",
-    img: "/public/images/image-7.webp",
+    img: "/images/image-7.webp",
   },
   {
     id: 8,
     title: "image-8",
-    img: "/public/images/image-8.webp",
+    img: "/images/image-8.webp",
   },
   {
     id: 9,
     title: "image-9",
-    img: "/public/images/image-9.webp",
+    img: "/images/image-9.webp",
   },
   {
     id: 10,
     title: "image-10",
-    img: "/public/images/image-10.jpeg",
+    img: "/images/image-10.jpeg",
   },
   {
     id: 11,
     title: "image-11",
-    img: "/public/images/image-11.jpeg",
+    img: "/images/image-11.jpeg",
   }
 ];
