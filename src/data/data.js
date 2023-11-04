88daa19e89782246987c556c3031c678424afef7
nomanfc <nomanfcb@gmail.com>
2023-11-04 23:17:38 +0600

    Some changes in Data file

---
+++ src/data/data.js
@@ -0,0 +1,57 @@
+export default [
+  {
+    id: 1,
+    title: "image-1",
+    img: "/images/image-1.webp",
+  },
+  {
+    id: 2,
+    title: "image-2",
+    img: "/images/image-2.webp",
+  },
+  {
+    id: 3,
+    title: "image-3",
+    img: "/images/image-3.webp",
+  },
+  {
+    id: 4,
+    title: "image-4",
+    img: "/images/image-4.webp",
+  },
+  {
+    id: 5,
+    title: "image-5",
+    img: "/images/image-5.webp",
+  },
+  {
+    id: 6,
+    title: "image-6",
+    img: "/images/image-6.webp",
+  },
+  {
+    id: 7,
+    title: "image-7",
+    img: "/images/image-7.webp",
+  },
+  {
+    id: 8,
+    title: "image-8",
+    img: "/images/image-8.webp",
+  },
+  {
+    id: 9,
+    title: "image-9",
+    img: "/images/image-9.webp",
+  },
+  {
+    id: 10,
+    title: "image-10",
+    img: "/images/image-10.jpeg",
+  },
+  {
+    id: 11,
+    title: "image-11",
+    img: "/images/image-11.jpeg",
+  }
+];
