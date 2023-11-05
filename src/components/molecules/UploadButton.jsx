aca34bd53b9b090872c8e7ae568175fc5164e700
nomanfc <nomanfcb@gmail.com>
2023-11-06 00:43:18 +0600

    Upload icon file path has been changed

--- src/components/molecules/UploadButton.jsx
+++ src/components/molecules/UploadButton.jsx
@@ -5,7 +5,7 @@ function ImageUploadButton() {
   return (
     <div className={styles.imageUploadContainer}>
       <label htmlFor="file" className={styles.imageUploadButton}>
-        <img src="/icons/upload.png" alt="Upload Icon" className={styles.imageUploadIcon} />
+        <img src="./icons/upload.png" alt="Upload Icon" className={styles.imageUploadIcon} />
         <span className={styles.imageUploadLabel}>Add Image</span>
       </label>
       <input type="file" id="file" accept="image/*" style={{ display: 'none' }} />
