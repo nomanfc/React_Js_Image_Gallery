ea9fcf2072a72ef5345f5bfb7383861603d6dc3d
nomanfc <nomanfcb@gmail.com>
2023-11-06 05:02:44 +0600

    Code format change in UploadButton

--- src/components/molecules/UploadButton.jsx
+++ src/components/molecules/UploadButton.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import styles from './molecules.module.css';
+import React from "react";
+import styles from "./molecules.module.css";
 
 function ImageUploadButton() {
   return (
     <div className={styles.imageUploadContainer}>
       <label htmlFor="file" className={styles.imageUploadButton}>
-        <img src="./icons/upload.png" alt="Upload Icon" className={styles.imageUploadIcon} />
+        <img
+          src="./icons/upload.png"
+          alt="Upload Icon"
+          className={styles.imageUploadIcon}
+        />
         <span className={styles.imageUploadLabel}>Add Image</span>
       </label>
-      <input type="file" id="file" accept="image/*" style={{ display: 'none' }} />
+      <input
+        type="file"
+        id="file"
+        accept="image/*"
+        style={{ display: "none" }}
+      />
     </div>
   );
 }
