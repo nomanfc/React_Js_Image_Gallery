8fc1cb3a02fd60b13d4b2e01ec476e298e6b6b2a
nomanfc <nomanfcb@gmail.com>
2023-11-05 22:46:10 +0600

    Navbar component has been separated from gallery

--- src/components/organisms/NavBar.jsx
+++ src/components/organisms/NavBar.jsx
@@ -1,9 +1,25 @@
-import styles from "../../styles/navBar.module.css";
+import styles from "../../styles/gallery.module.css";
 
-export default function NavBar() {
+export default function NavBar({ removeSelectedImages, selectedImages }) {
+  let countSelection = selectedImages.length;
+
   return (
     <div>
-      <div>HELLO</div>
+      <div className={styles.navbar}>
+        <div>
+          <div style={{ display: countSelection < 1 ? "block" : "none" }}>
+            Image Gallery
+          </div>
+          <div style={{ display: countSelection < 1 ? "none" : "block" }}>
+            {countSelection > 1
+              ? countSelection + " Files Selected"
+              : countSelection + " File Selected"}
+          </div>
+        </div>
+        <div>
+          <button onClick={removeSelectedImages}>Remove</button>
+        </div>
+      </div>
     </div>
   );
 }
