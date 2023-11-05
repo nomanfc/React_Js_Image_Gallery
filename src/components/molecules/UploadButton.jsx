a74b39af174300fd06a25ed1238c34c06fb27a66
nomanfc <nomanfcb@gmail.com>
2023-11-05 22:44:57 +0600

    Upload button has been added

---
+++ src/components/molecules/UploadButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './molecules.module.css';
+
+function ImageUploadButton() {
+  return (
+    <div className={styles.imageUploadContainer}>
+      <label htmlFor="file" className={styles.imageUploadButton}>
+        <img src="/icons/upload.png" alt="Upload Icon" className={styles.imageUploadIcon} />
+        <span className={styles.imageUploadLabel}>Add Image</span>
+      </label>
+      <input type="file" id="file" accept="image/*" style={{ display: 'none' }} />
+    </div>
+  );
+}
+
+export default ImageUploadButton;
