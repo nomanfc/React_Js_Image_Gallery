e669712cca77d376649a566b06486e08800b6032
nomanfc <nomanfcb@gmail.com>
2023-11-06 15:51:21 +0600

    useImageDragAndDrop: drag and drop logic updated

--- src/CustomHooks/useImageDragAndDrop.js
+++ src/CustomHooks/useImageDragAndDrop.js
@@ -3,23 +3,14 @@ import { useDrag, useDrop } from "react-dnd";
 export function useImageDragAndDrop(id, index, moveImage, ref) {
   const [, drop] = useDrop({
     accept: "image",
-    hover: (item, monitor) => {
+    hover: (item) => {
       const dragIndex = item.index;
       if (dragIndex === index) {
         return;
       }
-
       const hoverIndex = index;
-      const hoverBoundingRect = ref.current?.getBoundingClientRect();
-      const hoverMiddleY =
-        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-      const clientOffset = monitor.getClientOffset();
-      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
 
-      if (
-        (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) ||
-        (dragIndex > hoverIndex && hoverClientY > hoverMiddleY)
-      ) {
+      if (dragIndex !== hoverIndex) {
         moveImage(dragIndex, hoverIndex);
         item.index = hoverIndex;
       }
