3cf508cf02c2d00db5740586da216fcd243fcb8b
nomanfc <nomanfcb@gmail.com>
2023-11-06 15:55:32 +0600

    Navbar code has been refactored

--- src/components/organisms/NavBar.jsx
+++ src/components/organisms/NavBar.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { FaCheckSquare } from "react-icons/fa";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 import Button from "../atoms/Button";
@@ -5,53 +6,58 @@ import styles from "../../styles/styles.module.css";
 import useShowMessage from "../../CustomHooks/useShowMessage";
 
 export default function NavBar({ removeSelectedImages, selectedImages }) {
-  let countSelection = selectedImages.length;
+  const { showMessage, handleRemoveSelectedImages } =
+    useShowMessage(removeSelectedImages);
+  const countSelection = selectedImages.length;
 
-  const {showMessage, handleRemoveSelectedImages} = useShowMessage(removeSelectedImages);
+  const renderTitle = () => (
+    <div
+      className={styles.title}
+      style={{ display: countSelection < 1 && !showMessage ? "flex" : "none" }}
+    >
+      Image Gallery
+    </div>
+  );
+
+  const renderText = () => (
+    <div
+      className={styles.text}
+      style={{ display: countSelection < 1 ? "none" : "flex" }}
+    >
+      <FaCheckSquare
+        style={{ color: "green", fontSize: "20px", paddingRight: "10px" }}
+      />
+      {countSelection > 1
+        ? `${countSelection} Files Selected`
+        : `${countSelection} File Selected`}
+    </div>
+  );
+
+  const renderDeleteMessage = () => (
+    <div
+      className={styles.textDelete}
+      style={{ display: showMessage ? "flex" : "none" }}
+    >
+      <BsFillCheckCircleFill
+        style={{ fontSize: "20px", paddingRight: "10px" }}
+      />
+      Deletion Complete
+    </div>
+  );
 
   return (
-    <div>
-      <div className={styles.navbar}>
-        <div>
-          <div
-            className={styles.textDelete}
-            style={{ display: showMessage ? "flex" : "none" }}
-          >
-            <BsFillCheckCircleFill
-              style={{ fontSize: "20px", paddingRight: "10px" }}
-            />
-
-            {"Deletion Complete"}
-          </div>
-
-          <div
-            className={styles.title}
-            style={{
-              display: countSelection < 1 && !showMessage ? "flex" : "none",
-            }}
-          >
-            Image Gallery
-          </div>
-          <div
-            className={styles.text}
-            style={{ display: countSelection < 1 ? "none" : "flex" }}
-          >
-            <FaCheckSquare
-              style={{ color: "green", fontSize: "20px", paddingRight: "10px" }}
-            />
-
-            {countSelection > 1
-              ? countSelection + "  Files Selected"
-              : countSelection + " File Selected"}
-          </div>
-        </div>
-        <div style={{ height: "35px" }}>
-          <Button
-            title="Delete"
-            onClick={handleRemoveSelectedImages}
-            countSelection={countSelection}
-          />
-        </div>
+    <div className={styles.navbar}>
+      <div>
+        {renderDeleteMessage()}
+        {renderTitle()}
+        {renderText()}
+      </div>
+      <div style={{ height: "35px" }}>
+        <Button
+          title="Delete"
+          onClick={handleRemoveSelectedImages}
+          countSelection={countSelection}
+        />
       </div>
     </div>
   );
